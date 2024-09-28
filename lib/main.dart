295e69030d6c8e6883902e8a0036d62730f54790
tarus <victortarus6@gmail.com>
2024-09-28 17:22:48 +0300

    fixed title section

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
-import 'package:coffee_card/home.dart';
+import 'package:coffee_card/titlesection.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(home: Home()));
-}
+void main() => runApp(const MyApp());
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'Flutter layout demo';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text(appTitle),
+          ),
+          body: const SingleChildScrollView(
+            child: Column(
+              children: [
+                TitleSection(
+                    name: 'Oeschinen Lake Campground',
+                    location: 'Kandersteg, Switzerland')
+              ],
+            ),
+          )),
+    );
+  }
+}
--- lib/titlesection.dart
+++ lib/titlesection.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 class TitleSection extends StatelessWidget {
-  const TitleSection({super.key});
-
+  const TitleSection({super.key, required this.name, required this.location});
+  final String name;
+  final String location;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const  Padding(padding: EdgeInsets.all(32),
+    child:Row(
+      children: [
+      
+      ],
+    ),);
   }
 }
