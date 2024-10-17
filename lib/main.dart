dc322457a8692e6284311a22a8c99749c097dc87
tarus <victortarus6@gmail.com>
2024-10-17 17:16:44 +0300

    layout done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:coffee_card/titlesection.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -10,20 +9,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     const String appTitle = 'Flutter layout demo';
     return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
+        title: appTitle,
+        home: Scaffold(
           appBar: AppBar(
             title: const Text(appTitle),
           ),
-          body: const SingleChildScrollView(
-            child: Column(
-              children: [
-                TitleSection(
-                    name: 'Oeschinen Lake Campground',
-                    location: 'Kandersteg, Switzerland')
-              ],
-            ),
-          )),
-    );
+          body: const Center(
+            child: Text('Hello World'),
+          ),
+        ));
   }
 }
--- lib/titlesection.dart
+++ lib/titlesection.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
 
 class TitleSection extends StatelessWidget {
-  const TitleSection({super.key, required this.name, required this.location});
-  final String name;
-  final String location;
+  const TitleSection({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const  Padding(padding: EdgeInsets.all(32),
-    child:Row(
-      children: [
-      
-      ],
-    ),);
+    return const Placeholder();
   }
 }
